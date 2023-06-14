576fde6020ba411733b467bd8281955858dca6ea
Caleb Cochrane <Calebcochranebil33@gmail.com>
2023-06-14 10:56:31 -0700

    dataStorage-wifi

--- App.js
+++ App.js
@@ -3,12 +3,32 @@ import Chat from "./components/Chat";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import {
+    getFirestore,
+    disableNetwork,
+    enableNetwork,
+} from "firebase/firestore";
+import { useNetInfo } from "@react-native-community/netinfo"; // works like a React Hook
+import { useEffect } from "react";
+import { Alert } from "react-native";
 
 // Create the navigator
 const Stack = createNativeStackNavigator();
 
 const App = () => {
+    // this defines a new state that represents the network connectivity status
+    const connectionStatus = useNetInfo();
+
+    // displays an alert popup if connection is lost
+    useEffect(() => {
+        if (connectionStatus.isConnected === false) {
+            Alert.alert("Connection lost!");
+            disableNetwork(db);
+        } else if (connectionStatus.isConnected === true) {
+            enableNetwork(db);
+        }
+    }, [connectionStatus.isConnected]);
+
     const firebaseConfig = {
         apiKey: "AIzaSyDF7z7TRP_OvHOVZmYMNWMvEOOuomPXx7E",
         authDomain: "chat-app-fe7b9.firebaseapp.com",
@@ -29,7 +49,13 @@ const App = () => {
             <Stack.Navigator initialRouteName="Start">
                 <Stack.Screen name="Start" component={Start} />
                 <Stack.Screen name="Chat">
-                    {(props) => <Chat db={db} {...props} />}
+                    {(props) => (
+                        <Chat
+                            isConnected={connectionStatus.isConnected}
+                            db={db}
+                            {...props}
+                        />
+                    )}
                 </Stack.Screen>
             </Stack.Navigator>
         </NavigationContainer>
--- components/Chat.js
+++ components/Chat.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
-import { GiftedChat, Bubble } from "react-native-gifted-chat";
+import { GiftedChat, Bubble, InputToolbar } from "react-native-gifted-chat";
 import {
     collection,
     query,
@@ -8,37 +8,62 @@ import {
     onSnapshot,
     addDoc,
 } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 // onSnapshot implements real time communication by acting as a listener
 
-const Chat = ({ route, navigation, db }) => {
+const Chat = ({ route, navigation, db, isConnected }) => {
     const { name, color, userID } = route.params;
     const [messages, setMessages] = useState([]);
-    // state's setter function accepts a call back function with parameter refering to last set value of the state (previousMessages)
     const onSend = (newMessages) => {
         addDoc(collection(db, "messages"), newMessages[0]);
     };
 
+    // stores messages to localStorage (asyncStorage)
+    const loadCachedMessages = async () => {
+        const cachedMessages = (await AsyncStorage.getItem("messages")) || [];
+        setMessages(JSON.parse(cachedMessages));
+    };
+    // same logic as when using localStorage, and "try-catch" is an error handler
+    const cacheMessages = async (messagesToCache) => {
+        try {
+            await AsyncStorage.setItem(
+                "messages",
+                JSON.stringify(messagesToCache)
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
+    // must declare outside of the useEffect function so that the reference to old onSnapShot () is not lost
+    let unsubscribe;
+
+    // if there's a connection: fetch messages from firebase, if not: load from Async storage (loadCachedMessages)
     useEffect(() => {
-        const q = query(
-            collection(db, "messages"),
-            orderBy("createdAt", "desc")
-        );
-        const unsubscribe = onSnapshot(q, (querySnapshot) => {
-            const newMessages = [];
-            querySnapshot.forEach((doc) => {
-                const newMessage = doc.data();
-                newMessage.createdAt = new Date(
-                    newMessage.createdAt.toMillis()
-                );
-                newMessages.push(newMessage);
+        if (isConnected === true) {
+            if (unsubscribe) unsubscribe();
+            unsubscribe = null;
+            const q = query(
+                collection(db, "messages"),
+                orderBy("createdAt", "desc")
+            );
+            unsubscribe = onSnapshot(q, (querySnapshot) => {
+                const newMessages = [];
+                querySnapshot.forEach((doc) => {
+                    const newMessage = doc.data();
+                    newMessage.createdAt = new Date(
+                        newMessage.createdAt.toMillis()
+                    );
+                    newMessages.push(newMessage);
+                });
+                cacheMessages(newMessages);
+                setMessages(newMessages);
             });
-            setMessages(newMessages);
-        });
+        } else loadCachedMessages();
         return () => {
-            unsubscribe();
+            if (unsubscribe) unsubscribe();
         };
-    }, []);
+    }, [isConnected]);
 
     useEffect(() => {
         navigation.setOptions({ title: name });
@@ -60,14 +85,21 @@ const Chat = ({ route, navigation, db }) => {
         );
     };
 
+    const renderInputToolbar = (props) => {
+        if (isConnected) return <InputToolbar {...props} />;
+        else return null;
+    };
+
     return (
         <View style={[styles.container, { backgroundColor: color }]}>
             <GiftedChat
                 messages={messages}
                 renderBubble={renderBubble}
+                renderInputToolbar={renderInputToolbar}
                 onSend={(messages) => onSend(messages)}
                 user={{
                     _id: userID,
+                    avatar: "https://picsum.photos/140",
                     name: name,
                 }}
             />
@@ -82,6 +114,7 @@ const Chat = ({ route, navigation, db }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        padding: 15,
     },
 });
 
